613f80b6c1684896fb36455c8a95e6a66ec6641d
MaysenTG <greenwood.maysen@gmail.com>
2021-08-30 23:06:27 +1200

    Deploy whole app to github.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/keymap/sublime';
+import 'codemirror/theme/monokai.css';
+
+class MarkdownEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleHTMLChange = this.handleHTMLChange.bind(this);
+    this.handleCSSChange = this.handleCSSChange.bind(this);
+    this.state = { htmlValue: '<H1>This is a live code editor</H1>',  cssValue: 'H1 {\n   color: red;\n}\n'};
+  }
+
+  handleHTMLChange(e) {
+    //this.setState({ htmlValue: CodeMirror.getValue });
+     this.setState({ htmlValue: e.getValue('') });
+  }
+  
+  handleCSSChange(e) {
+    this.setState({ cssValue: e.getValue('') });
+  }
+  
+  useEffect() {
+    CodeMirror.closeTags();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="editorBox" id="htmlBox">
+          <p className="editorLabel">HTML</p>
+          <CodeMirror value={this.state.htmlValue} options={{theme: 'monokai', keyMap: 'sublime', mode: 'html', autoCloseTags: true, autoCloseBrackets: true, matchTags: false}} onChange={this.handleHTMLChange}/>
+        </div>
+        
+        <div className="editorBox" id="cssBox">
+          <p className="editorLabel">CSS</p>
+          <CodeMirror value={this.state.cssValue} options={{theme: 'monokai', keyMap: 'sublime', mode: 'css'}} onChange={this.handleCSSChange}/>
+        </div>
+        
+        <div id="codePreview" className="editorBox">
+          <p className="editorLabel">Live View</p>
+          <iframe title="CodePreview" id="iframePreview" srcDoc={"<html><head><style>"+this.state.cssValue + "</style></head><body>"+this.state.htmlValue+"</body</html>"} src="preview.html">test</iframe>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default MarkdownEditor;
